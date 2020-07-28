d08f84597a5378d6b0db9876b7cb194b522f6f26
Geoff Fox <fox.geoffrey@gmail.com>
2020-07-27 22:56:45 -0700

    Sending Responses

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
 const http = require("http");
+const { write } = require("fs");
 
 function rqListener(req, res) {}
 /* Now use the thick arrow func notation */
 const server = http.createServer((req, res) => {
-  console.log(req.url, req.method, req.headers);  
+  console.log(req.url, req.method, req.headers);
+  
+  res.setHeader('Content-Type', 'text/html');
+  res.write('<html>');
+  res.write('<head><title>My first psge</title></head>');
+  res.write('<body><h1>Hello world, from node server!</h1></body>');
+  res.write('</html>');
+  res.end();
 });
 
 server.listen(3000);
--- readme.md
+++ readme.md
@@ -38,3 +38,16 @@ PS C:\Users\foxge\Github\nodejs-complete-guide> node app.js
 ```
 
 ### Task: Sending Responses
+
+```Json
+const server = http.createServer((req, res) => {
+  console.log(req.url, req.method, req.headers);
+  
+  res.setHeader('Content-Type', 'text/html');
+  res.write('<html>');
+  res.write('<head><title>My first psge</title></head>');
+  res.write('<body><h1>Hello world, from node server!</h1></body>');
+  res.write('</html>');
+  res.end();
+});
+```
