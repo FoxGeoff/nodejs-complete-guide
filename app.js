aa93bf390700d1cb68d71e092f11cfa1b0ff72da
Geoff Fox <fox.geoffrey@gmail.com>
2020-07-29 16:30:01 -0700

    Adding 404 Error Page

--- app.js
+++ app.js
@@ -12,6 +12,10 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(adminRoutes); // not calling as func adminRoutes()
 app.use(shopRoutes);
 
+app.use((req, res,next) => { 
+    res.status(404).send('<h1>404 Error. Page not found!</h1>')
+});
+
 const server = http.createServer(app);
 
 server.listen(3000);
--- readme.md
+++ readme.md
@@ -406,3 +406,13 @@ router.get("/add-product", (req, res, next) => {
 
 module.exports = router;
 ```
+
+### Task: Adding 404 Error Page
+
+- App.js
+
+```Javascript
+app.use((req, res,next) => {
+    res.status(404).send('<h1>404 Error. Page not found!</h1>')
+});
+```
