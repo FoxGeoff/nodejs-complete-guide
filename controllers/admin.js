1ba4f5d66b007dc88c92eea665fb04316f5c4c56
Geoff Fox <fox.geoffrey@gmail.com>
2020-08-09 16:52:12 -0700

    Using Query Params

--- controllers/admin.js
+++ controllers/admin.js
@@ -20,9 +20,16 @@ exports.postAddProduct = (req, res, next) => {
 };
 
 exports.getEditProduct = (req, res, next) => {
+  const editMode = req.query.edit; // string 'true'
+  
+  if (!editMode) {
+    return res.redirect('/');
+  }
+
   res.render("admin/edit-product", {
-    pageTitle: "Add Product",
+    pageTitle: "Edit Product",
     path: "/admin/edit-product",
+    editing: editMode
   });
 };
 
