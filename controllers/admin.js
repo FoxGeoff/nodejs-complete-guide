92c231b71f96afa765bed8761bd967dbd76f3ee8
Geoff Fox <fox.geoffrey@gmail.com>
2020-08-14 16:09:17 -0700

    Pre-Populating the Edit Product Page with Data

--- controllers/admin.js
+++ controllers/admin.js
@@ -4,6 +4,7 @@ exports.getAddProduct = (req, res, next) => {
   res.render("admin/edit-product", {
     pageTitle: "Add Product",
     path: "/admin/add-product",
+    editing: false,
   });
 };
 
@@ -21,16 +22,30 @@ exports.postAddProduct = (req, res, next) => {
 
 exports.getEditProduct = (req, res, next) => {
   const editMode = req.query.edit; // string 'true'
-  
-  if (!editMode) {
-    return res.redirect('/');
-  }
 
-  res.render("admin/edit-product", {
-    pageTitle: "Edit Product",
-    path: "/admin/edit-product",
-    editing: editMode
+  /*
+  if (!editMode) {
+    return res.redirect("/");
+  } */
+  
+  const prodId = req.params.productId;
+  Product.findById(prodId, product => {
+
+    if (!product) {
+      /* `Error: Product Id: ${prodId} not found` */
+      return res.redirect("/");
+    }
+
+    res.render("admin/edit-product", {
+      pageTitle: "Edit Product",
+      path: "/admin/edit-product",
+      editing: true,
+      product: product
+    });
+
   });
+
+  
 };
 
 exports.getProducts = (req, res, next) => {
