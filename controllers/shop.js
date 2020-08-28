89fe3315d82ab91f44c7e6479cba86602c1698f1
Geoff Fox <fox.geoffrey@gmail.com>
2020-08-28 16:19:13 -0700

    Deleting Cart Items

--- controllers/shop.js
+++ controllers/shop.js
@@ -62,14 +62,18 @@ exports.getCart = (req, res, next) => {
 
 exports.postCart = (req, res, next) => {
   const prodId = req.body.productId;
-  Product.findById(prodId, product => { 
-    Cart.addProduct(prodId, product.price)  
+  Product.findById(prodId, (product) => {
+    Cart.addProduct(prodId, product.price);
   });
   res.redirect("/cart");
 };
 
-exports.postCartDelete = (req, res, next) => { 
-  //TBD
+exports.postCartDeleteProduct = (req, res, next) => {
+  const prodId = req.body.productId;
+  Product.findById(prodId, (product) => {
+    Cart.deleteProduct(prodId, product.price);
+    res.redirect("/cart");
+  });
 };
 
 exports.getOrders = (req, res, next) => {
